9929b3853ecb79e1c4a0f5781b843cb99c744494
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-11-10 11:13:26 +0800

    練習 2：新增資料-用save方式

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    $post=new Post();
+    $post->title ='test title';
+    $post->content ='test content';
+    $post->save();
 });
+
+
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
 Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])->name('posts.contact');
 Route::get('about', [\App\Http\Controllers\PostsController::class, 'about'])->name('posts.about');
+
+
